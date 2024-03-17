6b13a7a919161a6d62c92d6e08bfdab1885acb5a
esalathe <119804528+esalathe@users.noreply.github.com>
2024-03-17 16:33:36 +0100

    Add files via upload

--- network.py
+++ network.py
@@ -4,12 +4,16 @@ import torch.nn as nn
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super(NeuralNetwork, self).__init__()
-        self.fc1 = nn.Linear(6, 20)  # Input layer to hidden layer
-        self.fc2 = nn.Linear(20, 2)  # Hidden layer to output layer
+        self.fc1 = nn.Linear(6, 10)  # Input layer to first hidden layer
+        self.fc2 = nn.Linear(10, 10)  # First hidden layer to second hidden layer
+        self.fc3 = nn.Linear(10, 10)  # Second hidden layer to third hidden layer
+        self.fc4 = nn.Linear(10, 2)  # Third hidden layer to output layer
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))  # Apply ReLU activation function to the output of the first layer
-        x = self.fc2(x)  # Output layer (no activation function applied)
+        x = torch.relu(self.fc2(x))  # Apply ReLU activation function to the output of the second layer
+        x = torch.relu(self.fc3(x))  # Apply ReLU activation function to the output of the third layer
+        x = self.fc4(x)  # Output layer (no activation function applied)
         return x
 
 def create_nn():
