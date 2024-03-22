9638b12b8ab4365d42c65ee72875ed9ce0da17af
salim150 <mendozasalim150@gmail.com>
2024-03-22 19:50:43 +0100

    Update dynamics.py

--- dynamics.py
+++ dynamics.py
@@ -1,4 +1,4 @@
-import math
+import numpy as np
 
 class ObjectMovement:
     def __init__(self, x, y, speed, angle):
@@ -11,7 +11,7 @@ class ObjectMovement:
         # Apply constraints on maximum change in speed and angle
         max_delta_speed = 0.2  # Maximum change in speed
         max_speed = 1
-        max_delta_angle = math.radians(20)  # Maximum change in angle (in radians)
+        max_delta_angle = np.radians(20)  # Maximum change in angle (in radians)
 
         # Apply constraints on change in speed and angle
         delta_speed = min(max(delta_speed, -max_delta_speed), max_delta_speed)
@@ -22,8 +22,8 @@ class ObjectMovement:
         self.angle += delta_angle
 
         # Update x and y coordinates based on speed and angle
-        self.x += self.speed * math.cos(self.angle)
-        self.y += self.speed * math.sin(self.angle)
+        self.x += self.speed * np.cos(self.angle)
+        self.y += self.speed * np.sin(self.angle)
 
         return self.x, self.y, self.speed, self.angle
 
@@ -31,11 +31,11 @@ class ObjectMovement:
 initial_x = 0
 initial_y = 0
 initial_speed = 0.3
-initial_angle = math.radians(45)  # Angle in radians (45 degrees)
+initial_angle = np.radians(45)  # Angle in radians (45 degrees)
 obj = ObjectMovement(initial_x, initial_y, initial_speed, initial_angle)
 
 # Change speed and angle
-new_x, new_y, new_speed, new_angle = obj.move_object(delta_speed=2, delta_angle=math.radians(20))
+new_x, new_y, new_speed, new_angle = obj.move_object(delta_speed=2, delta_angle=np.radians(20))
 print("New coordinates:", new_x, new_y)
 print("New speed:", new_speed)
 print("New angle (in radians):", new_angle)'''
