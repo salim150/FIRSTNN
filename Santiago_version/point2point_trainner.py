cefe645110e9f5fe60ca55320b044a8b9e9adff6
salim150 <mendozasalim150@gmail.com>
2024-03-25 13:55:53 +0100

    Update point2point_trainner.py

--- Santiago_version/point2point_trainner.py
+++ Santiago_version/point2point_trainner.py
@@ -6,13 +6,14 @@ import matplotlib.pyplot as plt
 from get_full_traj import trajectory
 from Network import NeuralNetwork
 from plot_trayectory import tray_plot
+from loss_fn import loss_fn
 
 
 
 def P2P_train(epochs,car_params,Lenght=20,LR=0.1,start_parameters=torch.tensor([0,0,0,0],dtype=torch.float32),target = torch.tensor(([[2],[2]]),dtype=torch.float32)):
   model = NeuralNetwork()
   optimizer = optim.SGD(model.parameters(), lr=LR)
-  loss_fn = nn.MSELoss()
+  criterion = loss_fn()
 
   for epoch in range(epochs):
 
@@ -22,7 +23,7 @@ def P2P_train(epochs,car_params,Lenght=20,LR=0.1,start_parameters=torch.tensor([
 
     tray_plot(trajector,target.squeeze())
 
-    loss= loss_fn(trajector.T.reshape(Lenght+1,2),target*(torch.ones_like(trajector).T.reshape(Lenght+1,2)))
+    loss= criterion(trajector.T.reshape(Lenght+1,2),target*(torch.ones_like(trajector).T.reshape(Lenght+1,2)))
 
     torch.autograd.set_detect_anomaly(True)
 
