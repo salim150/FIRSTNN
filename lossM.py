0f95a04d32d16ce9013c844afde0f39d34dcb9a7
mfabrepfl <mathias.fabre@epfl.ch>
2024-04-15 15:47:08 +0200

    .

--- lossM.py
+++ lossM.py
@@ -1,23 +1,4 @@
 import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import matplotlib.pyplot as plt
-
-class loss_fn(nn.Module):
-    def __init__(self):
-        super(loss_fn, self).__init__()
-
-    def forward(self, x, y):
-        # Calculate the Euclidean distance between each point in the trajectory and the end goal
-        C=nn.MSELoss()
-        loss = C(x,y)
-
-        return loss
-    
-
-"""
-import numpy as np
 
 # Définition des constantes
 alpha = 1
@@ -71,7 +52,7 @@ def loss_function(x_robot, y_robot):
 
     return loss
 
-"""
+
 
 
 
