a6312d28b31d4557ac8e417303a1a0351342ff3b
abcarayol <abbas.carayol@gmail.com>
2024-05-05 20:26:31 +0200

    -

--- Obstacle avoidance/network.py
+++ Obstacle avoidance/network.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch import functional as F
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
@@ -8,12 +9,13 @@ class NeuralNetwork(nn.Module):
         self.fc2 = nn.Linear(10, 10)  # First hidden layer to second hidden layer
         self.fc3 = nn.Linear(10, 10)  # Second hidden layer to third hidden layer
         self.fc4 = nn.Linear(10, 2)  # Third hidden layer to output layer
+        self.act = nn.Tanh()
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))  # Apply ReLU activation function to the output of the first layer
         x = torch.relu(self.fc2(x))  # Apply ReLU activation function to the output of the second layer
         x = torch.relu(self.fc3(x))  # Apply ReLU activation function to the output of the third layer
-        x = torch.sigmoid(self.fc4(x))  # Output layer (no activation function applied)
+        x = self.act(self.fc4(x))  # Output layer (no activation function applied)
         return x
 
 def create_nn():
--- Obstacle avoidance/obstacle_generator2.py
+++ Obstacle avoidance/obstacle_generator2.py
@@ -1,54 +1,26 @@
-#Obstacle generator - Should be imported into the main file to see how well the network can avoid obstacles
-
-#Import dependencies
-import matplotlib.pyplot as plt
-from matplotlib.patches import PathPatch
-from matplotlib.path import Path
 import numpy as np
-
-#Can and should be changed to fit wihtin the boundaries of the plot
-low_limit = -6
-upper_limit = 0
+from parameters import Params
+import torch
 
 #Class to generate obstacles 
 class Obstacle_generator():
-    def __init__(self, min_coord = low_limit, max_coord=upper_limit):
-        self.min_coord = min_coord
-        self.max_coord = max_coord
-    
-    # Want to randomly generate the coordinates for the obstacles
-    def generate_obstacle(self):
-        xo1 = np.random.uniform(low=self.min_coord, high=self.max_coord)
-        xo2 = xo1 + np.random.uniform(low=self.min_coord, high=self.max_coord)
-        yo1 = np.random.uniform(low=self.min_coord, high=self.max_coord)
-        yo2 = yo1 + np.random.uniform(low=self.min_coord, high=self.max_coord)
+    def __init__(self):
+        self.obssize = Params['obssize']
+        self.Xmin_coord = Params['Environment_limits'][0][0] + self.obssize
+        self.Xmax_coord = Params['Environment_limits'][0][1] - self.obssize
+        self.Ymin_coord = Params['Environment_limits'][1][0] + self.obssize
+        self.Ymax_coord = Params['Environment_limits'][1][1] - self.obssize
 
-        vertices = [(xo1, yo1), (xo1, yo2), (xo2, yo2), (xo2, yo1), (xo1, yo1)]  # Closing the path
+    def generate_obstacle(self, x_start, y_start, x_end, y_end):
+        while (True) :
+            # generating the object randomly
+            object_middle = [np.random.uniform(low=self.Xmin_coord, high=self.Xmax_coord), np.random.uniform(low=self.Ymin_coord, high=self.Ymax_coord)]
+            # checking if the object isn't on top of the starting or ending points
+            if (abs(x_start-object_middle[0])>self.obssize) and (abs(y_start-object_middle[1])>self.obssize) :
+                if (abs(x_end-object_middle[0])>self.obssize) and (abs(y_end-object_middle[1])>self.obssize) :
+                    break
+        return torch.tensor(object_middle)
 
-        codes = [Path.MOVETO, Path.LINETO, Path.LINETO, Path.LINETO, Path.CLOSEPOLY]
-
-        path = Path(vertices, codes)
-
-        pathpatch = PathPatch(path, facecolor='grey', edgecolor='black')  # Change facecolor to the desired fill color
-        #Choose colour differently from the colour of simulated trajectory 
-
-        return pathpatch
-
-
-class Obstacle_Avoidance: 
-    def __init__(self, obstacle):
-        self.obstacle = obstacle
-    #Come up with a way of how the vehicle should move when it is getting to close the obstacle
-    #How will the car dynamics be implemented to avoid the obstacle
-    def avoid_obstacle(self, x, y):
-        # Here you can implement your obstacle avoidance logic
-        # For example, you can modify the trajectory to avoid collision with the obstacle
-        if (x >= self.obstacle[0] + 0.5 and x <= self.obstacle[2] - 0.5) and (y >= self.obstacle[1] + 0.5 and y <= self.obstacle[3] - 0.5):
-            # If the current position is close to obstacle move away from it 
-            x += np.random.uniform(-1, 1)
-            y += np.random.uniform(-1, 1)
-        return x, y
- 
 
 
 
